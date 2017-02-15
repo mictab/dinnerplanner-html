a1a729d583ff105519a47945795ada7add832816
Eleton <eleton@gmail.com>
2017-02-15 14:53:58 +0100

    All views done

--- Lab2/js/model/dinnerModel.js
+++ Lab2/js/model/dinnerModel.js
@@ -6,7 +6,7 @@ var DinnerModel = function () {
     //TODO Lab 2 implement the data structure that will hold number of guest
     // and selected dinner options for dinner menu
 
-    this.numberOfGuests = 0;
+    this.numberOfGuests = 1;
     this.menu = [];
 
     this.setNumberOfGuests = function (num) {
--- js/app.js
+++ js/app.js
@@ -4,10 +4,6 @@ $(function() {
 	//And create the needed controllers and views
 	model.addDishToMenu(1);
 	model.addDishToMenu(2);
-	var exampleView;
-	if($("#view4").length){
-		exampleView = new View4($("#view4"), model);
-	}else if($("#view5").length){
-		exampleView = new View5($("#view5"), model);
-	}
+	var exampleView = new View6($("#view6"), model);
+
 });
