fe2f33d4e4ac5b64a3ef54122fa9132719ca02a6
Michel Tabari <michel@lifesum.com>
2017-02-15 14:59:25 +0100

    Remove redundant folder

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	//We instantiate our model
-	var model = new DinnerModel();
-	//And create the needed controllers and views
-	model.addDishToMenu(1);
-	model.addDishToMenu(2);
-	var exampleView = new View6($("#view6"), model);
-
-});
